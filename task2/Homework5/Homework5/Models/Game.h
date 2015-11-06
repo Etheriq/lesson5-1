817ca1eb40e61f5577a823b05c4433c2f082bbf5
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2015-11-07 00:56:06 +0200

    added Card model from hw#4

---
+++ task2/Homework5/Homework5/Models/Game.h
@@ -0,0 +1,27 @@
+//
+//  Game.h
+//  Matcho
+//
+//  Created by Anton Lookin on 10/27/15.
+//  Copyright © 2015 geekub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Deck.h"
+
+
+@interface Game : UIView
+
+- (instancetype)initWithCardCount:(NSUInteger)count
+						usingDeck:(Deck *)deck;
+
+- (void)chooseCardAtIndex:(NSUInteger)index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+- (PlayingCard *) playCardAtIndex: (NSUInteger) index;
+
+@property (nonatomic, readonly) NSInteger score;
+@property (assign, nonatomic) NSInteger currentScore;
+@property (strong, nonatomic) NSString *matchLog;
+
+@end
