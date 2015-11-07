5268afa6b7a0288f22a256dc1b181300311fdac8
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2015-11-08 00:14:29 +0200

    custom controller with random background color

---
+++ task2/Homework5/Homework5/CustomViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomViewController.h
+//  Homework5
+//
+//  Created by Yuriy T on 07.11.15.
+//  Copyright © 2015 Yuriy T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomViewController : UIViewController
+
+@end
